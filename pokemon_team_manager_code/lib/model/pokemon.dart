4e2acaa75327a51c4cc1209e477b6344e051cb07
Erik Martín Garzón <erikmarting1200@gmail.com>
2022-12-15 16:37:31 +0100

    json and model

--- pokemon_team_manager_code/lib/model/pokemon.dart
+++ pokemon_team_manager_code/lib/model/pokemon.dart
@@ -1,51 +1,48 @@
 import 'dart:convert';
 
-//import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
 class Pokemon {
   String name, type;
   String avatarUrl;
-  int hp, attack, defense, specialAttack, specialDefense, speed;
-  int weight;
+ // int hp, attack, defense, specialAttack, specialDefense, speed;
   
 
   Pokemon({
     required this.name,
     required this.type,
     required this.avatarUrl,
-    required this.hp,
-    required this.attack,
-    required this.defense,
-    required this.specialAttack,
-    required this.specialDefense,
-    required this.speed,
-    required this.weight,
+    // required this.hp,
+    // required this.attack,
+    // required this.defense,
+    // required this.specialAttack,
+    // required this.specialDefense,
+    // required this.speed,
     
   });
 
-  // Pokemon.fromJson(Map<String, dynamic> json)
-  //     : name = json["name"]["first"],
-  //       type = json["name"]["last"],
-  //       avatarUrl = json["picture"]["large"],
-  //       hp = json["dob"]["age"],
-  //       attack = json["email"],
-  //       defense = json["login"]["username"];
-  //       specialAttack = json["login"]["username"];
-  //       specialDefense = json["login"]["username"];
-  //       speed = json["login"]["username"];
-  //       weight = json["login"]["username"];
-
-  
+  Pokemon.fromJson(Map<String, dynamic> json)
+      : name = json["name"],
+        type = json["types"]["type"]["name"],
+        avatarUrl = json["sprites"]["front_default"];
+        // hp = json["dob"]["age"],
+        // attack = json["email"],
+        // defense = json["login"]["username"],
+        // specialAttack = json["login"]["username"],
+        // specialDefense = json["login"]["username"],
+        // speed = json["login"]["username"];
+         
 }
+//We are only using the 1st gen pokemons (151)
 
-// Future<List<User>> loadUserList([int numUsers = 50]) async {
-//   final url = Uri.parse("https://randomuser.me/api/?results=$numUsers");
-//   final response = await http.get(url);
-//   final json = jsonDecode(response.body);
-//   final List jsonUserList = json["results"];
-//   List<User> userList = [];
-//   for (final jsonUser in jsonUserList) {
-//     userList.add(User.fromJson(jsonUser));
-//   }
-//   return userList;
-// }
+ Future<List<Pokemon>> loadUserList([int numPokemons = 151]) async {
+   final url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$numPokemons/");
+   final response = await http.get(url);
+   final json = jsonDecode(response.body);
+   final List jsonPokemonList = json["results"];
+   List<Pokemon> userList = [];
+   for (final jsonPokemon in jsonPokemonList) {
+     userList.add(Pokemon.fromJson(jsonPokemon));
+   }
+   return userList;
+ }
