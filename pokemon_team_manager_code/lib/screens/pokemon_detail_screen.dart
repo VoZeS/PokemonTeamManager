090abc61952c5d9bcbf22a6f6137b4c0201b6d19
VoZeS <davidboces12@hotmail.com>
2023-01-28 20:30:59 +0100

    Teams

--- pokemon_team_manager_code/lib/screens/pokemon_detail_screen.dart
+++ pokemon_team_manager_code/lib/screens/pokemon_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -12,156 +13,176 @@ class PokemonDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final pokemon = ModalRoute.of(context)!.settings.arguments as Pokemon;
+    final pokemonId = ModalRoute.of(context)!.settings.arguments as String;
+    final db = FirebaseFirestore.instance;
+
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-            alignment: const Alignment(0.0, -0.525),
-            child: Transform.rotate(
-              angle: pi / 32,
-              child: Transform.scale(
-                scale: 1.1,
-                child: Container(
-                  color: const Color.fromARGB(255, 129, 238, 153),
-                  width: screenWidth,
-                  height: screenHeight / 3.5,
+    return StreamBuilder(
+      stream: db.doc("/Pokemons/$pokemonId").snapshots(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot,
+      ) {
+        // 1. Mirar si hi ha hagut errors
+        if (snapshot.hasError) {
+          return ErrorWidget(snapshot.error.toString());
+        }
+        // 2. Si encara no tenim dades
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        }
+        // Extreure dades del AsyncSnapshot
+        final docSnap = snapshot.data!;
+        return Scaffold(
+          body: Stack(
+            children: [
+              Align(
+                alignment: const Alignment(0.0, -0.525),
+                child: Transform.rotate(
+                  angle: pi / 32,
+                  child: Transform.scale(
+                    scale: 1.1,
+                    child: Container(
+                      color: const Color.fromARGB(255, 129, 238, 153),
+                      width: screenWidth,
+                      height: screenHeight / 3.5,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Align(
-            alignment: const Alignment(0.0, 0.525),
-            child: Transform.rotate(
-              angle: -pi / 32,
-              child: Transform.scale(
-                scale: 1.1,
-                child: Container(
-                  color: const Color.fromARGB(255, 255, 164, 164),
-                  width: screenWidth,
-                  height: screenHeight / 4,
+              Align(
+                alignment: const Alignment(0.0, 0.525),
+                child: Transform.rotate(
+                  angle: -pi / 32,
+                  child: Transform.scale(
+                    scale: 1.1,
+                    child: Container(
+                      color: const Color.fromARGB(255, 255, 164, 164),
+                      width: screenWidth,
+                      height: screenHeight / 4,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              SafeArea(
+                child: Column(children: [
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "POKEMON SELECTED",
+                      style: TextStyle(
+                          fontSize: 36,
+                          fontFamily: "Orbitron",
+                          fontWeight: FontWeight.w900),
+                    ), // Title
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        onPressed: (() {
+                          Navigator.of(context).pushNamed(
+                            '/pokedex',
+                          );
+                        }),
+                        icon: const Icon(Icons.arrow_back, color: Colors.black),
+                      ),
+                      Container(
+                        width: screenWidth - 100,
+                        height: 10,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 238, 238, 238)),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight / 20),
+                  CircleAvatar(
+                    backgroundImage: docSnap['avatar'] != null
+                        ? NetworkImage(docSnap['avatar']!)
+                        : null,
+                    backgroundColor: Colors.yellowAccent,
+                    radius: 100.0,
+                  ),
+                  SizedBox(height: screenHeight / 40),
+                  Center(
+                    child: Text(
+                      docSnap['name'],
+                      style: const TextStyle(
+                          fontSize: 30,
+                          fontFamily: "Orbitron",
+                          fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                  const Text(
+                    "Water",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: "Orbitron",
+                        fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(height: screenHeight / 8),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      SizedBox(height: 8),
+                      Text(
+                        "T. HP: 20",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.w100),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "T. Def: 10",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.w100),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "T. Att: 55",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.w100),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "T. S.Def: 60",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.w100),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "T. S.Att: 72",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.w100),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "T. Spd: 12",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.w100),
+                      ),
+                    ],
+                  )
+                ]),
+              ),
+            ],
           ),
-          SafeArea(
-            child: Column(children: [
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "POKEMON SELECTED",
-                  style: TextStyle(
-                      fontSize: 36,
-                      fontFamily: "Orbitron",
-                      fontWeight: FontWeight.w900),
-                ), // Title
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: (() {
-                      Navigator.of(context).pushNamed(
-                        '/pokedex',
-                      );
-                    }),
-                    icon: const Icon(Icons.arrow_back, color: Colors.black),
-                  ),
-                  Container(
-                    width: screenWidth - 100,
-                    height: 10,
-                    decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 238, 238, 238)),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight / 20),
-              CircleAvatar(
-                backgroundImage: pokemon.avatarUrl != null
-                    ? NetworkImage(pokemon.avatarUrl!)
-                    : null,
-                backgroundColor: Colors.yellowAccent,
-                radius: 100.0,
-              ),
-              SizedBox(height: screenHeight / 40),
-              Center(
-                child: Text(
-                  pokemon.name,
-                  style: const TextStyle(
-                      fontSize: 30,
-                      fontFamily: "Orbitron",
-                      fontWeight: FontWeight.w900),
-                ),
-              ),
-              const Text(
-                "Water",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: "Orbitron",
-                    fontWeight: FontWeight.w400),
-              ),
-              SizedBox(height: screenHeight / 8),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  SizedBox(height: 8),
-                  Text(
-                    "T. HP: 20",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: "Orbitron",
-                        fontWeight: FontWeight.w100),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "T. Def: 10",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: "Orbitron",
-                        fontWeight: FontWeight.w100),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "T. Att: 55",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: "Orbitron",
-                        fontWeight: FontWeight.w100),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "T. S.Def: 60",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: "Orbitron",
-                        fontWeight: FontWeight.w100),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "T. S.Att: 72",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: "Orbitron",
-                        fontWeight: FontWeight.w100),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "T. Spd: 12",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: "Orbitron",
-                        fontWeight: FontWeight.w100),
-                  ),
-                ],
-              )
-            ]),
-          ),
-        ],
-      ),
-      backgroundColor: const Color.fromARGB(255, 255, 235, 163),
+          backgroundColor: const Color.fromARGB(255, 255, 235, 163),
+        );
+      },
     );
   }
 }
--- pokemon_team_manager_code/lib/widgets/pokemon_list.dart
+++ pokemon_team_manager_code/lib/widgets/pokemon_list.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutterfire_ui/auth.dart'; 
+import 'package:flutterfire_ui/auth.dart';
 
 class PokemonList extends StatelessWidget {
   const PokemonList({super.key});
@@ -12,13 +12,13 @@ class PokemonList extends StatelessWidget {
 
     return StreamBuilder(
       stream: db
-        .collection("/Pokemons")
-        .orderBy("pokedexNum", descending: false)
-        .snapshots(),
+          .collection("/Pokemons")
+          .orderBy("pokedexNum", descending: false)
+          .snapshots(),
       builder: (
         BuildContext context,
         AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
-      ){
+      ) {
         if (snapshot.hasError) {
           return ErrorWidget(snapshot.error.toString());
         }
@@ -27,26 +27,29 @@ class PokemonList extends StatelessWidget {
         }
         final querySnap = snapshot.data!;
         final docs = querySnap.docs;
+
         return ListView.builder(
           itemCount: docs.length,
           itemBuilder: (context, index) {
             final doc = docs[index];
             final attack = (doc['attack'] as String);
-            final defense = (doc['defense'] as String); 
-            final pNumb=(doc['pokedexNum']as String);
-            final avatar=(doc['avatar']as String);
+            final defense = (doc['defense'] as String);
+            final pNumb = (doc['pokedexNum'] as String);
+            final avatar = (doc['avatar'] as String);
             return ListTile(
               leading: Text(pNumb.toString()),
               title: Text(doc['name']),
-              subtitle: Text("Att: ${attack.toString()} Def: ${defense.toString()}"),
+              subtitle:
+                  Text("Att: ${attack.toString()} Def: ${defense.toString()}"),
               trailing: CircleAvatar(
                 backgroundImage: NetworkImage(avatar),
-                maxRadius: 20,
-                minRadius: 20,
-                ),
+                maxRadius: 25,
+                minRadius: 25,
+              ),
               onTap: () {
-                //Llògica per afegir al equip el pokemon
-                
+                // Navigator.of(context).pushNamed(
+                //   '/pokemon-details',
+                // );
               },
             );
           },
